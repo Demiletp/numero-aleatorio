5e3e9e543eeb4aff7d2819ad094a52eed9932261
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-20 17:02:56 -0300

    fist commit2

--- js/batebank.ts
+++ js/batebank.ts
@@ -1,7 +1,9 @@
 let saldo =3000;
 
-const elementoSaldo = document.querySelector(".saldo-valor .valor");
-elementoSaldo.textContent = saldo;
+const elementoSaldo = document.querySelector(".saldo-valor .valor") as  HTMLElement;
+if(elementoSaldo != null){ 
+    elementoSaldo.textContent = saldo.toString();
+}
  
 const elementoFormulario = document.querySelector(".block-nova-transacao form");
 elementoFormulario.addEventListener("submit", function(envent){
