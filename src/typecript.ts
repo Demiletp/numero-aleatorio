4ce4ac9249e32dc662d06117ceccef329cc27949
Demiletp <141511721+Demiletp@users.noreply.github.com>
2024-06-28 21:55:10 -0300

    fist commit11

--- src/typecript.ts
+++ src/typecript.ts
@@ -1,6 +1,31 @@
+//Tipos de Variaveis Primitivas
 let valor: number = 3000;
 let nome: string = "meu nome é Julia"
 let isPago: boolean = false;
 let qualquer: any = "";
 
 qualquer = 22;
+
+//Arrays
+const lista: number[] = [];
+lista.push(12,13,14,15,1.54);
+
+//Tipos personalizados 
+type Transacao = {
+    tipoTransacao: string;
+    data:Date;
+    valor:number;
+}
+
+//Enum 
+enum tipoTransacao{
+    DEPOSITO = "Depósito",
+    TRANSFERENCIA = "Transferência",
+    PAGAMENTO_BOLETO = "Pagamento de Boleto"
+}
+
+const  novaTransacao: Transacao = {
+    tipoTransacao: "",
+    data: new Date(),
+    valor: 0 
+}
